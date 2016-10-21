daef3d08bfee5e3f4c8f53ecb3e4ef06740a52da
Dmitriy Serdtsev <in19farkt@gmail.com>
2016-10-21 18:54:51 +0700

    Search

---
+++ frontend/blocks/search/search.js
@@ -0,0 +1,36 @@
+'use strict';
+
+$(function () {
+
+    function search(text) {
+        //ищем text и возвращаем результат
+        console.log('Serching')
+    }
+
+    $('.search').each(function () {
+
+        let $search = $(this);
+        let $text = $('.search__text', $search);
+
+        $('.search__submit', $search).on('click', function (event) {
+            if ($text.val() == '') return;
+
+            let result = search($text.val());
+            if (result) {
+                //выводим результат
+                return;
+            }
+
+            $search.addClass('search_error');
+            $text.val('');
+            $text.attr('placeholder', 'I\'ve not found what I\'m looking for...');
+        });
+
+        $('.search__text', $search).on('focusin', function (event) {
+            if (!$search.hasClass('search_error')) return;
+
+            $search.removeClass('search_error');
+            $text.attr('placeholder', 'Search');
+        })
+    })
+});
