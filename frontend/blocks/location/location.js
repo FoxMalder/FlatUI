904f3e55a0f73aea02cbe66e47a9b0648c8aa79e
Dmitriy Serdtsev <in19farkt@gmail.com>
2016-10-28 17:58:29 +0700

    Add block Location

---
+++ frontend/blocks/location/location.js
@@ -0,0 +1,31 @@
+'use strict'
+
+$(function () {
+
+    $('.location').each(function () {
+        let coord;
+        try {
+            coord = JSON.parse($(this).data('coord'));
+        }
+        catch(e) {
+            coord = [56.4531907, 84.9756513]
+        }
+
+        ymaps.ready(() => {
+            let map = new ymaps.Map($('.location__ymap', $(this))[0], {
+                center: coord,
+                zoom: 15,
+                controls: []
+            });
+
+            let placemark = new ymaps.Placemark(coord, {}, {
+                iconLayout: 'default#image',
+                iconImageHref: require('./img/map-marker.png'),
+                iconImageSize: [56, 56],
+                iconImageOffset: [-19, -56]
+            });
+
+            map.geoObjects.add(placemark);
+        });
+    })
+});
