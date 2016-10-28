96f0fc0ec54864f0fe6d8a9395e9997c2e4874c0
Dmitriy Serdtsev <in19farkt@gmail.com>
2016-10-28 12:49:31 +0700

    Change block Stages

--- frontend/blocks/stages/stages.js
+++ frontend/blocks/stages/stages.js
@@ -8,20 +8,5 @@ $(function () {
         let itemsLength = $('.stages__item', $(this)).length;
         $('.stages__progress', $(this))
             .css('width', (100 * (progress - 1) / (itemsLength - 1)) + '%');
-
-        $(this).on('change', function (event) {
-
-            progress = $(this).data('progress');
-            $('.stages__progress', $(this))
-                .css('width', (100 * (progress - 1) / (itemsLength - 1)) + '%');
-
-            let items = $('.stages__item', $(this));
-            for (i = 0; i < items.length; i++) {
-                if (progress < i)
-                    items[i].addClass('stages__item_completed');
-                else
-                    items[i].removeClass('stages__item_completed');
-            }
-        })
     })
 });
