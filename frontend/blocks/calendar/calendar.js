fedb5f797103d540c64f9a5d2555d65f32ff07cb
Dmitriy Serdtsev <in19farkt@gmail.com>
2016-10-30 18:22:21 +0700

    Add block Calendar

---
+++ frontend/blocks/calendar/calendar.js
@@ -0,0 +1,16 @@
+'use strict';
+
+$(function () {
+
+    $('.calendar').each(function () {
+
+        let $day = $('.calendar__day', $(this));
+
+        $('.calendar__widget', $(this)).datepicker({
+            changeYear: false,
+            altField: $day,
+            altFormat: "dd",
+            firstDay: 1
+        });
+    });
+});
