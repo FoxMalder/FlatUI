7ddd43f7a6303c13e0dc4e835a3ec320345f47b5
Dmitriy Serdtsev <in19farkt@gmail.com>
2016-10-31 19:16:11 +0700

    Add block Messager

---
+++ frontend/blocks/messager/messager.js
@@ -0,0 +1,23 @@
+'use strict';
+
+$(function () {
+
+    $('.messager').each(function () {
+
+        //скрываем скрол в чате
+        let $scroller = $('.messager__chat-scroller', $(this)),
+            $chat = $('.messager__chat', $(this)),
+            scrollWidth = $scroller.width() - $chat.width();
+
+        $scroller.css('margin-right', - scrollWidth + 'px');
+
+        $('.messager__btn-submit', $(this)).on('click', (event) => {
+            let message = $('.messager__input', $(this)).val();
+
+            if (message == '') return;
+
+            let $message = $('<div class="messager__message messager__message_out"><span>' + message + '</span></div>');
+            $chat.append($message);
+        })
+    })
+});
